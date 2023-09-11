36a05b6e373a1a8d57cdfc81a0466c3e44c52163
Adrienbrl <bruel.adrienpro@gmail.com>
2023-09-11 12:24:00 +0200

    Configuration des routes d'authentification

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,16 @@ const express = require ("express");
 const mongoose = require ("mongoose");
 const app = express ();
 const bookRoutes = require ("./routes/book");
+const userRoutes = require("./routes/user");
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log({ message: error }));
-  // Route qui gère les requêtes liés aux livre de l'API
+
+  // Route qui gère les requêtes liées aux livre de l'API
   app.use("/api/books", bookRoutes);
+
+  //Route qui gère les requêtes d'authentification de l'API
+  app.use("/api/auth", userRoutes);
+  
 module.exports = app;
